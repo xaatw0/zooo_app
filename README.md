52c3fadd26c41a077102e3dca28c341cc8d2e8c9
xaatw0 <xaatw0@gmail.com>
2024-11-08 20:11:00 +0900

    プロジェクトの作成

---
+++ README.md
@@ -0,0 +1,3 @@
+# zooo_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/github/xaatw0/zooo_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.github.xaatw0.zooo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
